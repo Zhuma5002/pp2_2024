8c5942f6bd8f4682a4101850f1662c05290ae289
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-22 21:35:22 +0600

    added variable names

--- week/week1/4.py
+++ week/week1/4.py
@@ -2,3 +2,18 @@ x=7
 y="Zhanik"
 print(x)
 print(y)
+
+#variable names
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
