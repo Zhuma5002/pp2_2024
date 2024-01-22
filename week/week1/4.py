2ed96d19b34a3e87d71054cba209768a6a646b2d
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-22 21:38:59 +0600

    added assign multyple values

--- week/week1/4.py
+++ week/week1/4.py
@@ -17,3 +17,15 @@ print(_my_var)
 print(myVar)
 print(MYVAR)
 print(myvar2)
+
+#assign multyple values
+
+x, y, z = "audi", "mercedes", "bmw"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Kazakhstan"
+print(x)
+print(y)
+print(z)
