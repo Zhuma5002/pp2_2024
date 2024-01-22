20342fcf6256e975882afd5500b244b5b08b47b8
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-22 21:48:53 +0600

    added slicing str

--- week/week1/8.py
+++ week/week1/8.py
@@ -8,3 +8,13 @@ print(x)
 #text
 x="hello everyone das ist my python practice"
 print(x)
+
+#slicing str
+x="auf wiedersehen"
+print(x[3:])
+
+x="auf wiedersehen"
+print(x[3:5])
+
+x="auf wiedersehen"
+print(x[:4])
