ea59abdf1ae379b67e6adce6ad86daa0df2b7902
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-22 22:06:33 +0600

    added format str

--- week/week1/8.py
+++ week/week1/8.py
@@ -41,3 +41,11 @@ a = "Hello"
 b = "World"
 c = a + " " + b
 print(c)
+
+#format str
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
