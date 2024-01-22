988d772faa14718ada11bc213e6b656c9dd2a52a
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-22 20:55:16 +0600

    added strings

--- week/week1/7.py
+++ week/week1/7.py
@@ -2,8 +2,23 @@
 x = int(1)   # x will be 1
 y = int(2.8) # y will be 2
 z = int("3") # z will be 3
+print(x)
+print(y)
+print(z)
+
 
 # floats
 x = int(1)   # x will be 1
 y = int(2.8) # y will be 2
 z = int("3") # z will be 3
+print(x)
+print(y)
+print(z)
+
+#strings
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+print(x)
+print(y)
+print(z)
