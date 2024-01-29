e570f00b4b9f59f8cfd5d0226a72131efbd82bf3
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-29 19:50:28 +0600

    added lists

---
+++ week/week2/4.py
@@ -0,0 +1,20 @@
+#python lists
+
+list=["cat", "dog", "fish"]
+print(list)
+
+list=["cat", "dog", "fish"]
+print(len(list))
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+print(list1)
+print(list2)
+print(list3)
+
+list1 = ["abc", 34, True, 40, "male"]
+print(list1)
+
+list=["cat", "dog", "fish"]
+print(type(list))
