666cb7cfd95d87526797a641ee87bce309d924f0
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-29 20:25:10 +0600

    added dictionaries

---
+++ week/week2/7.py
@@ -0,0 +1,16 @@
+# python dictionaries
+
+dict={"brand":"Ferrari","model":"laferrari","year":"2013"}
+print(dict)
+
+dict={"brand":"Ferrari","model":"laferrari","year":"2013"}
+print(dict["model"]) # dict items
+
+dict={"brand":"Ferrari","brand":"BMW","model":"laferrari","year":"2013"} # Dictionaries cannot have two items with the same key:
+print(dict)
+
+dict={"brand":"Ferrari","model":"laferrari","year":"2013"}
+print(len(dict))
+
+dict={"brand":"Ferrari","model":"laferrari","year":"2013"}
+print(type(dict))
