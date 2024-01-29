e04a80cc3dc0badbc8c58d7e6459aa6fa1ae25a5
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-29 19:59:50 +0600

    added tuples

---
+++ week/week2/5.py
@@ -0,0 +1,23 @@
+#tuples
+
+thistuple = ("Kazakhstan", "Kyrgyzstan", "Uzbekistan")
+print(thistuple)
+
+thistuple = ("Kazakhstan","Kazakhstan" "Kyrgyzstan", "Uzbekistan") #allowed dublicates
+print(thistuple)
+
+thistuple = ("Kazakhstan","Kazakhstan","Kyrgyzstan", "Uzbekistan") #tuple length
+print(len(thistuple))
+
+thistuple = ("Kazakhstan","Kazakhstan","Kyrgyzstan", "Uzbekistan")
+print(type(thistuple))
+
+thistuple = ("Kazakhstan") # not a tuple
+print(type(thistuple))
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+print(tuple1)
+print(tuple2)
+print(tuple3) # data types
