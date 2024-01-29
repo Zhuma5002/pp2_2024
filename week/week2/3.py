ef11bd085f3d5c15c59e1d4d170ecd098e5e9977
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-29 19:17:24 +0600

    added logical operators

--- week/week2/3.py
+++ week/week2/3.py
@@ -57,3 +57,17 @@ print(x > y) # returns True because 5 is greater than 3
 x = 5
 y = 3
 print(x >= y) # returns True because five is greater, or equal, to 3
+
+#logical operators:
+
+#and:
+x = 7
+print(x > 3 and x < 10) # returns True because 5 is greater than 3 AND 5 is less than 10
+
+#or:
+x = 5
+print(x > 3 or x < 4) # returns True because one of the conditions are true (5 is greater than 3, but 5 is not less than 4)
+
+#not:
+x = 5
+print(not(x > 3 and x < 10)) # returns False because not is used to reverse the result
