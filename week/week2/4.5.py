1806cae4863716ba34237901fce3475105954191
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-29 22:44:52 +0600

    added loop lists

---
+++ week/week2/4.5.py
@@ -0,0 +1,19 @@
+# loop lists
+
+# to prrinnt all items in the list,one  by one:
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
