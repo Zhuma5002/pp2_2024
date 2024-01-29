b97d0a7f15ddbe68893a10f573e3b4ec32e14260
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-29 22:14:36 +0600

    added list items

---
+++ week/week2/4.1.py
@@ -0,0 +1,10 @@
+#list items
+
+list=["cat", "dog", "fish"]
+print(list[1]) # it wil print the second item "dog"
+
+list=["cat", "dog", "fish"]
+print(list[-1]) # it will print the last item"fish"
+
+list=["cat", "dog", "fish"]
+print(list[0:2])
