4687dc9a688f00986cd1efa03795e924310d92b1
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-29 20:42:25 +0600

    added continue

--- week/week2/9.py
+++ week/week2/9.py
@@ -11,3 +11,10 @@ while i < 6:
   if (i == 3):
     break
   i += 1 # while break
+
+  i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i) # Note that number 3 is missing in the result "continue"
