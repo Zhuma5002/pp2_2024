eca5dba3bb7fb6c93136d3dc25e3f781ffb03886
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-29 20:50:34 +0600

    added break

--- week/week2/10.py
+++ week/week2/10.py
@@ -12,3 +12,9 @@ for x in fruits:
   print(x) 
   if x == "banana":
     break # break statement it will output apple banana
+  
+  fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x) #Exit the loop when x is "banana", but this time the break comes before the print
