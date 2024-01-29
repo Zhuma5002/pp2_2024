22ebc67a6e1e6eb09211272c8774737b45fddfe5
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-29 23:06:30 +0600

    added lists

--- week/week2/4.1.py
+++ week/week2/4.1.py
@@ -7,4 +7,15 @@ list=["cat", "dog", "fish"]
 print(list[-1]) # it will print the last item"fish"
 
 list=["cat", "dog", "fish"]
-print(list[0:2])
+print(list[0:2]) # it will return the first and the second item
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+
+#Negative indexing means starting from the end of the list.
+
+#This example returns the items from index -4 (included) to index -1 (excluded)
+
+#Remember that the last item has the index -1,
+
+
---
+++ week/week2/4.8.py
@@ -0,0 +1,11 @@
+#copy lists
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist) # Make a copy of a list with the copy() method
+
+
+# another method to make a copy is use"list" method:
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
