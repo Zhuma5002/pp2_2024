f6f72a2e4146d6077e59c6e4b23ab93d8cd147ff
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-29 19:37:05 +0600

    added membership operators

--- week/week2/3.py
+++ week/week2/3.py
@@ -87,3 +87,11 @@ z = x
 print(x is not z) # returns False because z is the same object as x
 print(x is not y) # returns True because x is not the same object as y, even if they have the same content
 print(x != y) # to demonstrate the difference betweeen "is not" and "!=": this comparison returns False because x is equal to y
+
+#memberdhip operators:
+
+x = ["Zhuma", "Aidos"]
+print("Aidos" in x) # returns True because a sequence with the value "banana" is in the list
+
+x = ["Pusha", "Baltimore"]
+print("Zhuma" not in x) # returns True because a sequence with the value "pineapple" is not in the list
