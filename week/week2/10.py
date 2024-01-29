fdc0f728b991f9511e39d9a8e0088c5a34ae2537
Zhumagali <z_amangaliyev@kbtu.kz>
2024-01-29 21:00:43 +0600

    added else in for loop

--- week/week2/10.py
+++ week/week2/10.py
@@ -31,9 +31,10 @@ for x in fruits:
   for x in range(2, 30, 5):
    print(x) # it will putput 2 7 12 17 22 27
 
-   for x in range(6):
-  if x == 3: break
-  print(x)
+  for x in range(6):
+   if x == 3: break
+   print(x)
 else:
-  print("Finally finished!") # else in for loop
+   print("Finally finished!") # else in for loop
+
 
