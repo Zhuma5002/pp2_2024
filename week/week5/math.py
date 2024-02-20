52d8aaa3b2a9a98da8a6f626f71f7ae41fef4099
Zhumagali <z_amangaliyev@kbtu.kz>
2024-02-20 20:54:18 +0600

    added math

---
+++ week/week5/math.py
@@ -0,0 +1,24 @@
+x=min(4,5,6,7)
+y=max(4,6,7,8)
+print(x)
+print(y)
+
+x=abs(-2332)
+print(x)
+
+x=pow(4,3)
+print(x)
+
+
+#math module:
+import math
+x=math.sqrt(64)
+print(x)
+
+x=math.ceil(1.4)
+y=math.floor(1.4)
+print(x) # returns 2
+print(y) # returns 1
+
+x=math.pi
+print(x)
